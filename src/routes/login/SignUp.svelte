ee0a51afbd12524fc09e9753a89219023162598a
SpectralCoder <mujahidcse20@gmail.com>
2023-06-01 02:41:12 +0600

    basic sign up form

---
+++ src/routes/login/SignUp.svelte
@@ -0,0 +1,120 @@
+<script>
+	export let flipped;
+
+	//need to create a loop for rendering these items
+</script>
+
+<div class="md:mx-6 md:px-12 md:py-4 w-full h-full">
+	<div class="w-full h-4 bg-gradient-to-r from-[#a8e063] to-[#56ab2f]" />
+	<form class="p-4">
+		<div class="grid grid-cols-2">
+			<div class="relative mb-4" data-te-input-wrapper-init>
+				<input
+					type="text"
+					class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+					id="exampleFormControlInput1"
+					placeholder="Sheikh Mujibur Rahman"
+				/>
+				<label
+					for="exampleFormControlInput1"
+					class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+					>Name
+				</label>
+			</div>
+			<div class="relative mb-4" data-te-input-wrapper-init>
+				<input
+					type="number"
+					class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+					id="exampleFormControlInput1"
+					placeholder="491202789140526"
+				/>
+				<label
+					for="exampleFormControlInput1"
+					class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+					>NID No.
+				</label>
+			</div>
+		</div>
+		<div class="grid grid-cols-2">
+			<div class="relative mb-4" data-te-input-wrapper-init>
+				<input
+					type="text"
+					class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+					id="exampleFormControlInput1"
+					placeholder="skmujibur@example.com"
+				/>
+				<label
+					for="exampleFormControlInput1"
+					class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+					>Email
+				</label>
+			</div>
+			<div class="relative mb-4" data-te-input-wrapper-init>
+				<input
+					type="number"
+					class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+					id="exampleFormControlInput1"
+					placeholder="+8801700432125"
+				/>
+				<label
+					for="exampleFormControlInput1"
+					class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+					>Phone No.
+				</label>
+			</div>
+		</div>
+		<div class="grid grid-cols-2">
+			<div class="relative mb-4" data-te-input-wrapper-init>
+				<input
+					type="password"
+					class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+					id="exampleFormControlInput1"
+					placeholder="skmujibur@example.com"
+				/>
+				<label
+					for="exampleFormControlInput1"
+					class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+					>Password
+				</label>
+			</div>
+			<div class="relative mb-4" data-te-input-wrapper-init>
+				<input
+					type="password"
+					class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+					id="exampleFormControlInput1"
+					placeholder="+8801700432125"
+				/>
+				<label
+					for="exampleFormControlInput1"
+					class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+					>Confirm password
+				</label>
+			</div>
+		</div>
+
+		<div class="mb-12 pb-1 pt-1 text-center w-full">
+			<button
+				class="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+				type="button"
+				data-te-ripple-init
+				data-te-ripple-color="light"
+				style="
+      background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+    "
+			>
+				sign up
+			</button>
+		</div>
+	</form>
+	<div class="flex items-center justify-between pb-6" on:click={() => (flipped = !flipped)}>
+		<p class="mb-0 mr-2">Already have an account?</p>
+		<button
+			type="button"
+			class="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+			data-te-ripple-init
+			data-te-ripple-color="light"
+		>
+			Login
+		</button>
+	</div>
+</div>
