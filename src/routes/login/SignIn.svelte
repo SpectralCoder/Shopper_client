9c91494d3b346c7b86b515893c94a42225f6b67e
Mujahidul Islam <mujahidcse20@gmail.com>
2023-07-22 02:32:08 +0600

    Generic sign up frontend stable (#2)

--- src/routes/login/SignIn.svelte
+++ src/routes/login/SignIn.svelte
@@ -1,5 +1,30 @@
 <script>
+	import Input from '$lib/components/Input.svelte';
+
 	export let flipped;
+
+	let data = [
+		{
+			key: 'email',
+			title: 'Email',
+			value: '',
+			placeholder: 'skmujibur@example.com',
+			type: 'text',
+			output_type: 'str'
+		},
+		{
+			key: 'password',
+			title: 'password',
+			value: '',
+			placeholder: 'password',
+			type: 'password',
+			output_type: 'str'
+		},
+	]
+
+	function submitData() {
+		console.log('data', data);
+	}
 </script>
 
 <div class="md:mx-6 md:px-12 md:py-4 flip-card-front">
@@ -7,54 +32,41 @@
 		<img class="mx-auto w-48" src="shopperFavIcon.svg" alt="logo" />
 		<h4 class="mb-8 mt-1 pb-1 text-2xl font-semibold">
 			<span
-				class="text-transparent text-3xl bg-clip-text bg-gradient-to-r from-[#a8e063] to-[#56ab2f]"
+				class="text-transparent text-3xl bg-clip-text text-[#576319]"
 				>Shopper :</span
 			> Unleash the True Potential of Your Business
 		</h4>
 	</div>
 
-	<form>
-		<p class="mb-4">Please login to your account</p>
+	<form class="lg:px-12">
 		<!--Username input-->
-		<div class="relative mb-4" data-te-input-wrapper-init>
-			<input
-				type="text"
-				class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-				id="exampleFormControlInput1"
-				placeholder="shopper@example.com"
+		{#each data as item}
+		<div class="relative mb-6">
+			<Input
+				type="{item.type}"
+				bind:inputValue={item.value}
+				placeholder={item.placeholder}
 			/>
+
 			<label
 				for="exampleFormControlInput1"
-				class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-				>Email
+				class="w-full pointer-events-none absolute left-0 top-0 mb-0 max-w-[100%] origin-[0_0] truncate pt-[-1rem] leading-[1.6] transition-all duration-200 text-left pl-2 py-2   {item.value
+					? 'scale-[0.8] text-[#576319] ease-out translate-y-[-1rem] motion-reduce:transition-none pl-0'
+					: 'border border-[#576319] rounded-lg peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-[#576319] peer-data-[te-input-state-active]:-translate-y-[0.9rem]'} peer-focus:border-none peer-focus:pl-0"
+				>{item.title}
 			</label>
 		</div>
-
-		<!--Password input-->
-		<div class="relative mb-4" data-te-input-wrapper-init>
-			<input
-				type="password"
-				class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-				id="exampleFormControlInput11"
-				placeholder="Password"
-			/>
-			<label
-				for="exampleFormControlInput11"
-				class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-				>Password
-			</label>
-		</div>
+	{/each}
 
 		<!--Submit button-->
 		<div class="mb-12 pb-1 pt-1 text-center">
 			<button
-				class="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+				class="mb-3 inline-block w-full bg-[#576319] rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
 				type="button"
 				data-te-ripple-init
 				data-te-ripple-color="light"
-				style="
-      background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
-    "
+				on:click={submitData}
+
 			>
 				Log in
 			</button>
--- src/routes/login/SignUp.svelte
+++ src/routes/login/SignUp.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Input from '../../lib/components/Input.svelte';
 	export let flipped;
-
+	let error = {};
 	let data = [
 		{
 			key: 'name',
@@ -53,58 +53,141 @@
 		}
 	];
 
-	function validateCheck(type, key, value) {
-		return;
-		if (type === 'int' && key === 'phone') {
-			//write code validate a bangladeshi phone number
-
+	function validateCheck(type, key, value, addErr = true) {
+		if (key == 'phone') {
+			const regex = /^01[1-9][0-9]{8}$/;
+			let parsedInt = parseInt(value)
+			console.log(!isNaN(parsedInt), regex.test(value))
+			if (!isNaN(parsedInt) && regex.test(value) && value) {
+				console.info(value);
+				if (key in error) {
+					delete error[key];
+				}
+			} else if (addErr) {
+				error[key] = 'Input a valid number';
+			}
+		} else if (key == 'email') {
+			const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+			if (emailPattern.test(value)) {
+				if (key in error) {
+					delete error[key];
+				}
+			} else if (addErr) {
+				error[key] = 'Input a valid email';
+			}
+		} else if (key == 'password') {
+			const lengthPattern = /^.{6,}$/;
+			const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
+			const numberPattern = /[0-9]/;
+			if (
+				lengthPattern.test(value) &&
+				specialCharPattern.test(value) &&
+				numberPattern.test(value)
+			) {
+				if (key in error) {
+					delete error[key];
+				}
+			} else if (addErr) {
+				error[key] =
+					'Input must be greater than 6 digit and have atleast a number and special character';
+			}
+		} else if (key == 'cpassword') {
+			let pass = '';
+			for (let i = 0; i < data.length; i++) {
+				if (data[i].key == 'password') {
+					pass = data[i].value;
+					break;
+				}
+			}
+			if (value == pass && value) {
+				if (key in error) {
+					delete error[key];
+				}
+			} else if (addErr) {
+				error[key] = "doesn't match with the password";
+			}
+		} else if (key == 'nid_no') {
+			let parsedInt = parseInt(value)
+			if (Number.isInteger(parsedInt)) {
+				if (key in error) {
+					console.info(error);
+					delete error[key];
+					console.info(error);
+				}
+			} else if(value === ""){
+				return;
+			}else if (addErr) {
+				error[key] = 'Please, Input a valid NID no.';
+			}
+		} else if (key == 'name') {
+			const regex = /^[A-Za-z\s']+$/;
+			if (regex.test(value)) {
+				if (key in error) {
+					delete error[key];
+				}
+			} else if (addErr) {
+				error[key] = 'Input a valid name';
+			}
 		}
+
+		error = error;
 	}
 
-	//need to create a loop for rendering these items
+	function submitData() {
+		for (let i = 0; i < data.length; i++) {
+			validateCheck(data[i].type, data[i].key, data[i].value);
+		}
+		console.info(data, error)
+		if (Object.keys(error).length > 0) return;
+		console.info('will submit data', data);
+	}
 </script>
 
 <div class="md:mx-6 md:px-12 md:py-4 w-full h-full">
-	<div class="w-full pt-6 mt-2 h-4 mb-2 bg-gradient-to-r from-[#a8e063] to-[#56ab2f]" />
+	<div class="w-full pt-6 mt-2 h-4 mb-2 bg-[#576319] opacity-80" />
 	<img class="mx-auto w-48 mb-2" src="shopperFavIcon.svg" alt="logo" />
-	<form class="p-4">
+	<form class="py-4">
 		<div class="grid grid-cols-2">
 			{#each data as item}
-				<div class="relative mb-6">
+				<div class="relative mb-6 px-3">
 					<Input
 						type="{item.type}"
 						bind:inputValue={item.value}
 						placeholder={item.placeholder}
-						onInput={() => validateCheck(item.type, item.key, item.value)}
+						onInput={() => validateCheck(item.type, item.key, item.value, false)}
 					/>
-
 					<label
 						for="exampleFormControlInput1"
 						class="w-full pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[-1rem] leading-[1.6] text-neutral-500 transition-all duration-200 text-left pl-2 py-2   {item.value
-							? 'scale-[0.8] ease-out text-primary translate-y-[-1rem] motion-reduce:transition-none pl-0'
-							: 'border border-primary rounded-lg peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem]'} peer-focus:border-none peer-focus:pl-0"
+							? "scale-[0.8] ease-out text-[#576319] translate-y-[-1rem] motion-reduce:transition-none pl-0"
+							: `border ${error[item.key]?"border-[#00718F]":"border-[#576319]"} rounded-lg peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-[#576319] peer-data-[te-input-state-active]:-translate-y-[0.9rem]`} peer-focus:border-none peer-focus:pl-0"
 						>{item.title}
 					</label>
+					{#if error[item.key] && item.value}
+					<div class="text-[#00718F] text-xs text-left">
+						{error[item.key]}
+					</div>
+						
+					{/if}
 				</div>
 			{/each}
 		</div>
 
 		<div class="mb-12 pb-1 pt-1 text-center w-full">
 			<button
-				class="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+				class="mb-3 inline-block bg-[#576319] w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
 				type="button"
 				data-te-ripple-init
 				data-te-ripple-color="light"
-				style="
-      background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
-    "
+				on:click={submitData}
 			>
 				sign up
 			</button>
 		</div>
 	</form>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<div class="flex items-center justify-between pb-6" on:click={() => (flipped = !flipped)}>
-		<p class="mb-0 mr-2 text-primary pl-6">Already have an account?</p>
+		<p class="mb-0 mr-2 text-[#576319] pl-6">Already have an account?</p>
 		<button
 			type="button"
 			class="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
